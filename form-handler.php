5595c5d1543b012e60b5e17bbb7fe0f831e5befe
Josh Brookshire <Josh@iMac.local>
2018-05-19 09:28:40 -0400

    form build out

--- form-handler.php
+++ form-handler.php
@@ -1,8 +1,10 @@
-<?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $say = htmlspecialchars($_POST['say']);
-  $to  = htmlspecialchars($_POST['to']);
+<html>
+<body>
 
-  echo  $say, ' ', $to;
-?>
+ <?php 
+ echo $_GET["site"]; 
+ ?>
+
+
+</body>
+</html>
