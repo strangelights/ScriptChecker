e7eb8c01681c391bcd82e2a131d95e95a0f38088
Josh Brookshire <Josh@iMac.local>
2018-07-30 22:25:49 -0400

    applying user-agent header to cURL

--- php/proxy.php
+++ php/proxy.php
@@ -3,7 +3,7 @@
 // this page will serve up HTML scraped via the form handler PHP and pass it back to the front end via AJAX
 
 // retrieve the form data by using the element's name attributes value as key 
-$site = $_POST["siteX"];
+$site = $_POST["siteURL"];
 
 $ch = curl_init("$site");
 $timeout = 5;
@@ -11,8 +11,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_FAILONERROR,TRUE);   
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout); // timeout set to 5 seconds
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);  // follow any 301 redirects to their destination
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'); //prevent 403 errors (current version of chrome)
 
-// execute!
 $response = curl_exec($ch);
 
 if(curl_errno($ch)) {
@@ -21,8 +21,6 @@ if(curl_errno($ch)) {
 
 // close the connection, release resources used
 curl_close($ch);
-
-// do anything you want with your response
 
 echo $response;
 
