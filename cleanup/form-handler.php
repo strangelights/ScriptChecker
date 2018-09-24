6f48e42cbfc2ccb2db6678e55057565a475f4aac
strangelights <jbrookshire7@gmail.com>
2018-09-24 08:58:02 -0400

    final prep for astabox

--- cleanup/form-handler.php
+++ cleanup/form-handler.php
@@ -1,11 +1,34 @@
 <?php
 // Check if the form is submitted 
-if ( isset( $GET["POST"] ) ) { 
+if ( isset( $_POST["submit"] ) ) { 
 // retrieve the form data by using the element's name attributes value as key 
-$site = $POST["site"]; 
-// display the results
-echo "You entered: >" . $site;
-//echo $_REQUEST['site']; 
-exit; 
+$site = $_POST["site"];
+echo "<h3>Hello World</h3>";
+echo "You entered:" . "$site";
+
+$ch = curl_init("$site");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+// execute!
+$response = curl_exec($ch);
+
+
+// check for execution errors
+
+if(curl_errno($ch)) {
+	echo "Whoops something went wrong! " . curl_error($ch);
+	exit;
 }
+
+// close the connection, release resources used
+curl_close($ch);
+
+// do anything you want with your response
+echo "$response";
+
+}
+
 ?>
+
+
+ /// DELETE THIS FILE /////
